e225f695c5159ffe7e30fa1ae4733ec7c8e39bb0
Thahsina <s.thahsina@gmail.com>
2022-01-06 11:36:51 +0300

    script js

--- script.js
+++ script.js
@@ -65,69 +65,7 @@ class Cycling extends Workout {
 }
 
 
-  constructor(coords, distance, duration) {
-    this.distance = distance;
-    this.coords = coords;
-    this.duration = duration;
-  }
-
-  _setDescription(){
-      //prettier-ignore
-      const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-
-      this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]}  ${this.date.getDate()}`;
-
-  }
-
-
-  click() {
-    this.clicks++;
-  }
-}
-
-class Running extends Workout {
-    type = 'running'
-  constructor(coords, distance, duration, cadence) {
-    super(coords, distance, duration);
-    this.cadence = cadence;
-    this.calcPace();
-    this._setDescription()
-  }
-
-  calcPace() {
-    this.pace = this.duration / this.distance;
-    return this.pace;
-  }
-}
-
-class Cycling extends Workout {
-    type = 'cycling'
-  constructor(coords, distance, duration, elevationGain) {
-    super(coords, distance, duration);
-    this.elevationGain = elevationGain;
-    this.calcSpeed();
-    this._setDescription()
-  }
-
-  calcSpeed() {
-    this.speed = this.distance / (this.duration / 60);
-    return this.speed;
-  }
-}
-
+  
 //-------------------APPLICATION ARCHITECTURE--------------------//
 
 const form = document.querySelector(".form");
